fe21c9e472dfa3017da3901d448941b1da7e4f15
Minh Phan <nminh238@gmail.com>
2021-04-08 09:54:19 +0700

    add unit test

--- src/com/example/lolmatchhistory/Main.java
+++ src/com/example/lolmatchhistory/Main.java
@@ -1,8 +1,22 @@
 package com.example.lolmatchhistory;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import junit.framework.JUnit4TestAdapter;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UnitTests.class
+})
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new JUnit4TestAdapter(Main.class);
     }
 }
---
+++ src/com/example/lolmatchhistory/UnitTests.java
@@ -0,0 +1,11 @@
+package com.example.lolmatchhistory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTests {
+    @Test
+    public void test(){
+    }
+}
